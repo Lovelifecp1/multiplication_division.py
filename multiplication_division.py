8fc619f556c25f9468f6748d2227fb7ecab78e3e
Lovelifecp1 <crystal.perez@csuglobal.edu>
2024-11-17 10:42:37 -0800

    Add files via upload

---
+++ multiplication_division.py
@@ -0,0 +1,10 @@
+num1 = 10
+num2 = 7
+
+multiplication_result = num1 * num2
+
+division_result = num1 / num2
+
+print("The result of multiplication is:", multiplication_result)
+print("The result of division is:", division_result)
+
